502507a595b6f94b3c6e02f56a5be3abe0201f14
Ko530 <khagiwar@oswego.edu>
2026-04-18 19:04:23 -0400

    Connected

--- reuseu-frontend/src/App.jsx
+++ reuseu-frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
 import WelcomePage from "./components/WelcomePage";
+import Login from "./components/Login";
+import Header from "./components/Header";
+
+function HeaderPage() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#05070b" }}>
+      <Header />
+    </div>
+  );
+}
 
 function App() {
-    const [started, setStarted] = useState(false);
-    return <WelcomePage onStart={() => setStarted(true)} />;
+    const [page, setPage] = useState("welcome");
+
+      if (page === "welcome") {
+        return <WelcomePage onStart={() => setPage("login")} />;
+      }
+
+      if (page === "login") {
+        return <Login onSuccess={() => setPage("header")} />;
+      }
+
+      return <HeaderPage />;
+//     const [started, setStarted] = useState(false);
+//     return <WelcomePage onStart={() => setStarted(true)} />;
 //     if(!started){
 //         return <WelcomePage onStart={() => setStarted(true)} />;
 //     }
