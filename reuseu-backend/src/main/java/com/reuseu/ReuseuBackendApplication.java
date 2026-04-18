7b2bbdaaf39e794f6d907c4a17f38f3641f10b38
oaksoe <ozaw@oswego.edu>
2026-04-18 13:31:11 -0400

    Initial commit

---
+++ reuseu-backend/src/main/java/com/reuseu/ReuseuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reuseu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReuseuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReuseuBackendApplication.class, args);
+	}
+
+}
---
+++ reuseu-backend/src/test/java/com/reuseu/ReuseuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reuseu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReuseuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
